2859b2cbf3d594ba6943f1336ebbd046a6116d90
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-25 17:08:15 -0400

    haha! getting the hang of this]

--- src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
+++ src/main/java/com/zipcodewilmington/streams/conversions/ArrayConverter.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.streams.conversions;
 import com.zipcodewilmington.streams.anthropoid.Person;
 import com.zipcodewilmington.streams.anthropoid.PersonFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,16 +26,19 @@ public final class ArrayConverter extends PersonConversionAgent<Person[]> {
 
     //TODO
     public List<Person> toList() {
-        return null;
+        List<Person> ans = new ArrayList<>();
+        return new ArrayList<>(ans);
     }
 
     //TODO
     public Stream<Person> toStream() {
-        return null;
+        List<Person> ans = new ArrayList<>();
+        return ans.stream();
     }
 
     @Override
     public Person[] toArray() {
-        return super.objectSequence;
+        List<Person> ans = new ArrayList<>();
+        return ans.stream().toArray(Person[] :: new);
     }
 }
